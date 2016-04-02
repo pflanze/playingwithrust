bc999d75493738348a5a3b1e45a2b5dd98d6df94
Christian Jaeger <ch@christianjaeger.ch>
2016-04-02 11:02:15 +0200

    works!

--- hello.rs
+++ hello.rs
@@ -4,7 +4,7 @@ use std::env;
 
 
 fn main () {
-    let x = env::args_os();
+    let mut x = env::args_os();
     println!("The second element is {}",
              x.nth(0).expect("no arg 0")
              .to_str().expect("not unicode"));
