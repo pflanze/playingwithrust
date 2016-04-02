996138fc775ed1ff1c6d12fb91a06f830fd72885
Christian Jaeger <ch@christianjaeger.ch>
2016-04-02 10:50:40 +0200

    ok now borrow stuff

--- hello.rs
+++ hello.rs
@@ -3,7 +3,6 @@ use std::env;
 
 fn main () {
     let x = env::args_os();
-    let a: [i32; 4] = [1, 2, 3, 4];     // As usual, the type annotation is optional.
-    
-    println!("The second element is {}", expect(x.nth(0), "no arg 0"));
+    println!("The second element is {}",
+             x.nth(0).expect("no arg 0").to_str().expect("not unicode"));
 }
