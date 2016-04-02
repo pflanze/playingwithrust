382c53a4a257f2bdcdd0961e6773f4474e0aabb3
Christian Jaeger <ch@christianjaeger.ch>
2016-04-02 10:35:47 +0200

    .
    hello.rs:6:42: 6:46 error: cannot index a value of type `std::env::ArgsOs`
    hello.rs:6     println!("The second element is {}", x[1]);
                                                        ^~~~
    <std macros>:2:25: 2:56 note: in this expansion of format_args!
    <std macros>:3:1: 3:54 note: in this expansion of print! (defined in <std macros>)
    hello.rs:6:5: 6:48 note: in this expansion of println! (defined in <std macros>)
    error: aborting due to previous error

--- hello.rs
+++ hello.rs
@@ -1,5 +1,7 @@
-
+use std::env::args_os;
 
 fn main () {
-    println!("Hello");
+    let x = args_os();
+    let a: [i32; 4] = [1, 2, 3, 4];     // As usual, the type annotation is optional.
+    println!("The second element is {}", x[1]);
 }
