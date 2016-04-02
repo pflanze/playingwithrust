e9aa6f411399286e1381fb8ba0ffe0f06a61cc9f
Christian Jaeger <ch@christianjaeger.ch>
2016-04-02 10:59:37 +0200

    .

--- hello.rs
+++ hello.rs
@@ -1,8 +1,11 @@
 use std::env;
 //use std::option::Option;
 
+
+
 fn main () {
     let x = env::args_os();
     println!("The second element is {}",
-             x.nth(0).expect("no arg 0").to_str().expect("not unicode"));
+             x.nth(0).expect("no arg 0")
+             .to_str().expect("not unicode"));
 }
